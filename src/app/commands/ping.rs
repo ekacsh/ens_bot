aec964771879e8e65d2e3370598138579b005b40
Pabolo Pires <24285633+ekacsh@users.noreply.github.com>
2025-08-23 17:22:18 -0300

    feat: change ping command for test purposes

--- src/app/commands/ping.rs
+++ src/app/commands/ping.rs
@@ -2,7 +2,7 @@ use crate::{Context, Error};
 
 #[poise::command(prefix_command, slash_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), Error> {
-    let response = "Pong!";
+    let response = "Pong!!";
     ctx.say(response).await?;
     Ok(())
 }
